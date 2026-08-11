a0047a5c66838cdedf2611edb14f45c96884509e
Bashar27689 <b.w.nl.sy@gmail.com>
2026-08-11 12:33:59 +0200

    add new adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,10 +6,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaMariaDb(
-  process.env.DATABASE_URL!,
-  
-);
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST!,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+  connectionLimit: 5,
+});
 
 export const prisma =
   globalForPrisma.prisma ??
