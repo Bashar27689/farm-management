c7faa6537e6453e1dad70233acac853bcc657de6
Bashar27689 <b.w.nl.sy@gmail.com>
2026-08-16 18:27:08 +0200

    update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../../generated/prisma/client";
 
@@ -8,15 +9,28 @@ const globalForPrisma = globalThis as unknown as {
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST!,
-  port: Number(process.env.DATABASE_PORT),
+  port: Number(process.env.DATABASE_PORT || 3306),
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
+
+  // عدد الاتصالات لكل Prisma Client
   connectionLimit: 5,
- ssl: { rejectUnauthorized: false },
-  allowPublicKeyRetrieval: true,
+
+  // مهلة انتظار الحصول على connection
+  acquireTimeout: 30000,
+
+  // مهلة إنشاء الاتصال
   connectTimeout: 10000,
 
+  // إغلاق الاتصالات الخاملة بعد فترة
+  idleTimeout: 300,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  allowPublicKeyRetrieval: true,
 });
 
 export const prisma =
@@ -24,8 +38,7 @@ export const prisma =
   new PrismaClient({
     adapter,
   });
-  
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+// مهم جدًا على Hostinger
+// نحتفظ بنفس Prisma Client حتى في Production
+globalForPrisma.prisma = prisma;
